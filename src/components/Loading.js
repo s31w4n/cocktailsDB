eba9f3639896298c57b70ae9f296b4f288b9aa91
s31w4n <sewan.khalni@gmail.com>
2023-03-12 23:17:26 +0330

    initial ccommit

---
+++ src/components/Loading.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Loading = () => {
+  return (
+    <div className="loader_holder">
+      <div className="tbl_black">
+        <div className="tbl_ver_algn">
+          <div className="one">
+            <p>Loading...</p> <span></span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Loading;
