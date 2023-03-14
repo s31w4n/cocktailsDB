2024f786f72e693b5b0750f434a51c3f2a2c8818
s31w4n <sewan.khalni@gmail.com>
2023-03-14 23:20:05 +0330

    issue with deploying

--- src/pages/SingleCocktail.js
+++ src/pages/SingleCocktail.js
@@ -9,53 +9,52 @@ const SingleCocktail = () => {
   const [loading, setLoading] = useState(false);
   const [cocktail, setCocktail] = useState(null);
 
-  async function getCocktail() {
-    setLoading(true);
-    try {
-      const response = await fetch(`${url}${id}`);
-      const data = await response.json();
-      if (data.drinks) {
-        const {
-          strDrink: name,
-          strDrinkThumb: image,
-          strAlcoholic: info,
-          strCategory: category,
-          strGlass: glass,
-          strInstructions: instructions,
-          strIngredient1,
-          strIngredient2,
-          strIngredient3,
-          strIngredient4,
-          strIngredient5,
-        } = data.drinks[0];
-        const ingredients = [
-          strIngredient1,
-          strIngredient2,
-          strIngredient3,
-          strIngredient4,
-          strIngredient5,
-        ];
-        const newCocktail = {
-          name,
-          image,
-          info,
-          category,
-          glass,
-          instructions,
-          ingredients,
-        };
-        setCocktail(newCocktail);
-      } else {
-        setCocktail(null);
-      }
-      setLoading(false);
-    } catch (error) {
-      console.log(error);
-      setLoading(false);
-    }
-  }
-
   useEffect(() => {
+    setLoading(true);
+    async function getCocktail() {
+      try {
+        const response = await fetch(`${url}${id}`);
+        const data = await response.json();
+        if (data.drinks) {
+          const {
+            strDrink: name,
+            strDrinkThumb: image,
+            strAlcoholic: info,
+            strCategory: category,
+            strGlass: glass,
+            strInstructions: instructions,
+            strIngredient1,
+            strIngredient2,
+            strIngredient3,
+            strIngredient4,
+            strIngredient5,
+          } = data.drinks[0];
+          const ingredients = [
+            strIngredient1,
+            strIngredient2,
+            strIngredient3,
+            strIngredient4,
+            strIngredient5,
+          ];
+          const newCocktail = {
+            name,
+            image,
+            info,
+            category,
+            glass,
+            instructions,
+            ingredients,
+          };
+          setCocktail(newCocktail);
+        } else {
+          setCocktail(null);
+        }
+        setLoading(false);
+      } catch (error) {
+        console.log(error);
+        setLoading(false);
+      }
+    }
     getCocktail();
     //eslint-disable-next-line
   }, [id]);
